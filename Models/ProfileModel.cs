176148a5d994f938f803c7848c4cfb49a3e6f71f
Enes <enes_kln@hotmail.com>
2020-12-31 15:25:41 +0300

    forgotten models added

---
+++ Models/ProfileModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myiotprojects.Models
+{
+    public class ProfileModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public DateTime ProfileCreated { get; set; }
+        public IEnumerable<Post> AllPosts { get; set; }
+        public ChangePasswordModel ChangePasswordModel { get; set; }
+    }
+}
