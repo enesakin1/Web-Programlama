11d825d101992053ee66f1d37b279e82fadc3e70
Enes <enes_kln@hotmail.com>
2020-12-31 15:18:51 +0300

    post views

---
+++ Models/PostListingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myiotprojects.Models
+{
+    public class PostListingModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string AuthorID { get; set; }
+        public DateTime DatePosted { get; set; }
+        public int RepliesCount { get; set; }
+
+        public string AuthorProfileImage { get; set; }
+    }
+}
---
+++ Models/SearchResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myiotprojects.Models
+{
+    public class SearchResultModel
+    {
+        public IEnumerable<PostListingModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
+        public bool EmptySearchResults { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
